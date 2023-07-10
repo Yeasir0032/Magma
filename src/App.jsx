a356065c79143cc8e514f018e840a4f923cef50a
Yeasir0032 <124646897+Yeasir0032@users.noreply.github.com>
2023-07-10 15:24:48 +0000

    Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
+import {Home} from "./Pages/Home"
+import {Notes} from "./Pages/Notes"
 
 export default function App() {
   return (
     <div>
-      Hey
+      <Router>
+        <nav className="navbar">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+             <li><Link to="/Notes">Notes</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Notes" element={<Notes/>}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ src/Pages/Notes.jsx
@@ -0,0 +1,7 @@
+export const Notes = () => {
+  return (<div>
+    <h1>This is a Notes page</h1>
+  
+  </div>
+);
+};
