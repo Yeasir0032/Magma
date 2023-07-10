bd22505410ec77c835596440e3c9d73263737ba5
Yeasir0032 <124646897+Yeasir0032@users.noreply.github.com>
2023-07-10 18:11:50 +0000

    Home page edit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,9 @@
+import "./pagutil.css"
+
 export const Home = () => {
   return (<div>
     <h1 id="main-heading">Magma</h1>
-    <p>Welcome to **Magma** </p>
+    <p>Welcome to Magma </p>
 
 </div>
 );
